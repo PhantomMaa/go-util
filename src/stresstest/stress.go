352c6f35ea52cdd1d24bdd065a1d182df49f44ea
hanxuan.mh <hanxuan.mh@taobao.com>
2013-12-26 18:34:35 +0800

    update stress

--- src/stresstest/stress.go
+++ src/stresstest/stress.go
@@ -1,30 +1,36 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"os"
-	"encoding/json"
-	"net/url"
-	"time"
-	"runtime"
-	"sync/atomic"
 	"flag"
-	"strconv"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"runtime"
+	"strings"
+	"sync/atomic"
+	"time"
 )
 
-const paramFile = "param.json"
-const variableFile = "variable.json"
-
 var count int32 = 0
+
+const (
+	content = `
+Useage :
+	stress [-option] filePath
+Options :
+	-p maxCPUs : how many process to run
+	-f urls.txt : file contains urls to be requested
+		`
+)
 
 type Error struct {
 	Err error
 	Msg string
 }
 type Param struct {
-	url string
-	params map[string]interface{}
+	url    string
+	params map[string]string
 }
 
 func (e *Error) Error() string {
@@ -50,52 +56,51 @@ func readFile(filepath string) ([]byte, error) {
 	file.Read(buffer)
 	return buffer, nil
 }
-func prepareParam(filepath string) (*Param, error) {
+func prepareParam(filepath string) ([]Param, error) {
 	buffer, err := readFile(filepath)
 	if err != nil {
 		return nil, err
 	}
-	var jsonData interface{}
-	err = json.Unmarshal(buffer, &jsonData)
-	if err != nil {
-		return nil, err
+
+	content := string(buffer)
+
+	lineTemp := strings.Split(content, "\n")
+	if lineTemp == nil || len(lineTemp) <= 0 {
+		return nil, &Error{nil, "file content is empty"}
 	}
-	jsonMap, ok := jsonData.(map[string]interface{})
-	if !ok {
-		return nil, &Error{nil, "转化成json格式出错"}
+	lineLength := len(lineTemp)
+	params := make([]Param, 0, lineLength)
+	for i := 0; i < lineLength; i++ {
+		line := lineTemp[i]
+		param := Param{}
+		param.params = make(map[string]string)
+		temp := strings.Split(line, "?")
+		if len(temp) == 2 {
+			param.url = temp[0]
+			variables := temp[1]
+			variablesTemp := strings.Split(variables, "&")
+			for _, varibale := range variablesTemp {
+				keyValueTemp := strings.Split(varibale, "=")
+				param.params[keyValueTemp[0]] = keyValueTemp[1]
+			}
+			params = append(params, param)
+		}
 	}
-	param := Param{}
-	param.url, _ = jsonMap["url"].(string)
-	param.params, _ = jsonMap["params"].(map[string]interface{})
-	return &param, nil
-}
-func prepareVariable(filepath string) ([]interface{}, error) {
-	buffer, err := readFile(filepath)
-	if err != nil {
-		return nil, err
-	}
-	var jsonData interface{}
-	err = json.Unmarshal(buffer, &jsonData)
-	if err != nil {
-		return nil, err
-	}
-	jsonMap, ok := jsonData.(map[string]interface{})
-	if !ok {
-		return nil, &Error{nil, "转化成json格式出错"}
-	}
-	variables, ok := jsonMap["variables"].([]interface{})
-	if !ok {
-		return nil, &Error{nil, "转化variables格式出错"}
-	}
-	return variables, nil
+	return params, nil
 }
 func request(client *http.Client, param Param) {
 	time1 := time.Now().UnixNano()
 	form := url.Values{}
 	for key, value := range param.params {
-		form.Add(key, value.(string))
+		form.Add(key, value)
 	}
-	response, _ := client.PostForm(param.url, form)
+	response, err := client.PostForm(param.url, form)
+	if err != nil {
+		fmt.Println(param.url)
+		fmt.Println(form)
+		fmt.Printf("%s\n", err)
+		return
+	}
 	defer response.Body.Close()
 	if response.StatusCode != 200 {
 		fmt.Printf("response.code = %d, param : %s\n", response.StatusCode, param)
@@ -105,52 +110,37 @@ func request(client *http.Client, param Param) {
 	costTime := (time2 - time1) / (1000000)
 	fmt.Printf("%d, costTime = %d\n", count, costTime)
 }
-
-func loop(param Param, variables []interface{}) {
+func loop(params []Param) {
 	var client = &http.Client{}
 	for true {
-		if variables != nil {
-			for _, value := range variables {
-				valueMap, ok := value.(map[string]interface {})
-				if ok {
-					param.params = valueMap
-					request(client, param)
-				}
-			}
-		} else {
+		for _, param := range params {
 			request(client, param)
 		}
 	}
 }
+
 func main() {
-	numCPU := runtime.NumCPU()
-	runtime.GOMAXPROCS(numCPU)
+	var processerNum = runtime.NumCPU()
+	runtime.GOMAXPROCS(processerNum)
+
+	flag.IntVar(&processerNum, "p", processerNum, "how many processer to run")
+	filePath := flag.String("f", "urls.txt", "file contains urls to be requested")
 
 	flag.Parse()
-	args := flag.Args()
-	if len(args) != 1 {
-		content := `
-useage :
-	go run main.go parallelNum
-		`
-		fmt.Println(content)
-		os.Exit(1)
-	}
-	parallelNum, _ := strconv.Atoi(args[0])
-	endFlag := make(chan int)
-	param, err := prepareParam(paramFile)
+
+	params, err := prepareParam(*filePath)
 	if err != nil {
 		fmt.Println(err)
+		fmt.Println(content)
 		os.Exit(1)
 		return
 	}
-	variables, err := prepareVariable(variableFile)
-	if err != nil || variables == nil {
-		fmt.Println("没有配置请求参数变量，将以固定的参数发送请求")
+	endFlag := make(chan int)
+
+	fmt.Printf("start %d process\n", processerNum)
+	for i := 0; i < processerNum; i++ {
+		go loop(params)
 	}
-	for i := 0; i < parallelNum; i++ {
-		go loop(*param, variables)
-	}
-	<- endFlag
+	<-endFlag
 
 }
